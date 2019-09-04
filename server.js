c02f8203cb2eb9acbd022451ad2acc480830e39f
lilyhoratio <lilyhzhou@gmail.com>
2019-09-04 07:49:08 -0700

    GET request to posts

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.get(`/`, (req, res) => {
   res.status(200).json({ api: "I'm up..." });
 });
 
-// POST request to /api/posts
+// 1. POST request to /api/posts
 server.post(`/api/posts`, (req, res) => {
   //   const postInfo = req.body;
   //   //   console.log(postInfo);
@@ -23,25 +23,63 @@ server.post(`/api/posts`, (req, res) => {
   const { title, contents } = req.body;
 
   if (title && contents) {
-    // db.insert({ title, contents }).then(ids => ({ id: ids[0] }));
     db.insert({ title, contents })
       .then(({ id }) => {
         db.findById(id)
-          .then(user => {
-            res.status(201).json(user); // return HTTP status code 201 & newly created post
+          .then(post => {
+            res.status(201).json(post[0]); // return HTTP status code 201 & newly created post
           })
           .catch(err => {
             console.log(err);
-            res.status(500).json({ error: "server error retrieving post" });
+            res
+              .status(500)
+              .json({ error: "There was a server error retrieving the post" });
           });
       })
       .catch(err => {
         console.log(err);
-        res.status(500).json({ error: "server error inserting post" });
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
       });
   } else {
-    res.status(400).json({ error: "Requires title and contents of post" });
+    res
+      .status(400)
+      .json({ error: "Please provide title and contents for the post." });
   }
 });
+
+// 2. POST request to /api/posts/:id/comments
+server.post(`/api/posts/:id/comments`, (req, res) => {});
+
+// 3. When the client makes a GET request to /api/posts
+server.get(`/api/posts`, (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+// 4. When the client makes a GET request to /api/posts/:id:
+// - If the _post_ with the specified `id` is not found:
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON object: `{ message: "The post with the specified ID does not exist." }`.
+
+server.get(`/api/posts:id`, (req, res) => {
+  db.findById();
+});
+
+// 5. When the client makes a GET request to /api/posts/:id/comments
+
+// 6. When the client makes a DELETE request to /api/posts/:id
+
+// 7. When the client makes a PUT request to /api/posts/:id
 
 module.exports = server;
