c1284bad963314f31f56cea801ea87bcac4db9da
lilyhoratio <lilyhzhou@gmail.com>
2019-09-05 12:17:36 -0700

    updated 'start' to 'node index.js'

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const server = require("./server.js");
 
 const port = process.env.PORT || 8000; // read from Heroku PORT if exists, else default to 8000
 
-server.listen(port, () => console.log(`\nAPI on ${port}`));
+server.listen(port, () => console.log(`\nAPI on ${port}!!!`));
