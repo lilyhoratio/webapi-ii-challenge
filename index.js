0687e9c4fe64485f472f00ada2b85adb1c31044e
lilyhoratio <lilyhzhou@gmail.com>
2019-09-05 11:55:14 -0700

    added port var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const server = require("./server.js");
 
-const port = process.env.PORT || 8000; // read from Heroku PORT if exists, else default to 8000
+const port = process.env.PORT || 5000; // read from Heroku PORT if exists, else default to 8000
 
 server.listen(port, () => console.log(`\nAPI on ${port}`));
